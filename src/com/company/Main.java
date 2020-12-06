89348e3071ed4b9c61131e333931c9eb7499a96a
hiccupboi <denis.shulimovv@gmail.com>
2020-12-06 17:27:29 +0300

    2 commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,69 +1,54 @@
 package com.company;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Image;
-import java.awt.Toolkit;
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.BorderFactory;
-import javax.swing.Box;
-import javax.swing.ButtonGroup;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JRadioButton;
-import javax.swing.JTextField;
 @SuppressWarnings("serial")
-// Главный класс приложения, он же класс фрейма
-public class MainFrame extends JFrame {
+
+public class Main extends JFrame {
     // Размеры окна приложения в виде констант
     private static final int WIDTH = 400;
     private static final int HEIGHT = 320;
     // Текстовые поля для считывания значений переменных,
 // как компоненты, совместно используемые в различных методах
-    private JTextField textFieldX;
-    private JTextField textFieldY;
+    private final JTextField textFieldX;
+    private final JTextField textFieldY;
     // Текстовое поле для отображения результата,
 // как компонент, совместно используемый в различных методах
-    private JTextField textFieldResult;
+    private final JTextField textFieldResult;
     // Группа радио-кнопок для обеспечения уникальности выделения в группе
-    private ButtonGroup radioButtons = new ButtonGroup();
+    private final ButtonGroup radioButtons = new ButtonGroup();
     // Контейнер для отображения радио-кнопок
     private Box hboxFormulaType = Box.createHorizontalBox();
     private int formulaId = 1;
+
     // Формула №1 для рассчѐта
     public Double calculate1(Double x, Double y) {
-        return x*x + y*y;
+        return x * x + y * y;
     }
+
     // Формула №2 для рассчѐта
     public Double calculate2(Double x, Double y) {
-        return x*x*x + 1/y;
+        return x * x * x + 1 / y;
     }
+
     // Вспомогательный метод для добавления кнопок на панель
     private void addRadioButton(String buttonName, final int formulaId) {
         JRadioButton button = new JRadioButton(buttonName);
-        button.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent ev) {
-                MainFrame.this.formulaId = formulaId;
-                imagePane.updateUI();
-            }
-        });
+        button.addActionListener(ev -> Main.this.formulaId = formulaId);
         radioButtons.add(button);
         hboxFormulaType.add(button);
     }
+
     // Конструктор класса
-    public MainFrame() {
+    public Main() {
         super("Вычисление формулы");
         setSize(WIDTH, HEIGHT);
         Toolkit kit = Toolkit.getDefaultToolkit();
 // Отцентрировать окно приложения на экране
-        setLocation((kit.getScreenSize().width - WIDTH)/2,
-                (kit.getScreenSize().height - HEIGHT)/2);
+        setLocation((kit.getScreenSize().width - WIDTH) / 2,
+                (kit.getScreenSize().height - HEIGHT) / 2);
         hboxFormulaType.add(Box.createHorizontalGlue());
         addRadioButton("Формула 1", 1);
         addRadioButton("Формула 2", 2);
@@ -106,31 +91,27 @@ public class MainFrame extends JFrame {
         hboxResult.setBorder(BorderFactory.createLineBorder(Color.BLUE));
 // Создать область для кнопок
         JButton buttonCalc = new JButton("Вычислить");
-        buttonCalc.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent ev) {
-                try {
-                    Double x = Double.parseDouble(textFieldX.getText());
-                    Double y = Double.parseDouble(textFieldY.getText());
-                    Double result;
-                    if (formulaId==1)
-                        result = calculate1(x, y);
-                    else
-                        result = calculate2(x, y);
-                    textFieldResult.setText(result.toString());
-                } catch (NumberFormatException ex) {
-                    JOptionPane.showMessageDialog(MainFrame.this,
-                            "Ошибка в формате записи числа с плавающей точкой", "Ошибочный формат числа",
-                            JOptionPane.WARNING_MESSAGE);
-                }
+        buttonCalc.addActionListener(ev -> {
+            try {
+                Double x = Double.parseDouble(textFieldX.getText());
+                Double y = Double.parseDouble(textFieldY.getText());
+                Double result;
+                if (formulaId == 1)
+                    result = calculate1(x, y);
+                else
+                    result = calculate2(x, y);
+                textFieldResult.setText(result.toString());
+            } catch (NumberFormatException ex) {
+                JOptionPane.showMessageDialog(Main.this,
+                        "Ошибка в формате записи числа с плавающей точкой", "Ошибочный формат числа",
+                        JOptionPane.WARNING_MESSAGE);
             }
         });
         JButton buttonReset = new JButton("Очистить поля");
-        buttonReset.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent ev) {
-                textFieldX.setText("0");
-                textFieldY.setText("0");
-                textFieldResult.setText("0");
-            }
+        buttonReset.addActionListener(ev -> {
+            textFieldX.setText("0");
+            textFieldY.setText("0");
+            textFieldResult.setText("0");
         });
         Box hboxButtons = Box.createHorizontalBox();
         hboxButtons.add(Box.createHorizontalGlue());
@@ -150,9 +131,8 @@ public class MainFrame extends JFrame {
         contentBox.add(Box.createVerticalGlue());
         getContentPane().add(contentBox, BorderLayout.CENTER);
     }
-    // Главный метод класса
     public static void main(String[] args) {
-        MainFrame frame = new MainFrame();
+        Main frame = new Main();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
---
+++ src/com/company/MainFrame.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import javax.swing.*;
+
+public class MainFrame {
+    public static void main(String[] args) {
+        Main frame = new Main();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+    }
